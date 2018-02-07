e51a916009eba5571a2693e4c7bd73a9f2472f58
rudyknowak <rudyknowak@gmail.com>
2018-02-07 12:48:05 +0100

    poprawka 1

--- src/ArrayComparator.java
+++ src/ArrayComparator.java
@@ -1,22 +1,17 @@
 public class ArrayComparator {
-    public void compare (int [][] arrayA, int [][] arrayB ) {
+
+    public boolean compare(int[][] arrayA, int[][] arrayB) {
         boolean equalOrNot = true;
 
-        if (arrayA.length == arrayB.length) {
-            for (int i = 0; i < arrayA.length; i++)
-                for (int j = 0; j < arrayB.length; j++)
-                    if (arrayA[i][j] == arrayB[i][j]) {
-                        equalOrNot = true;
-                    } else {
-                        equalOrNot = false;
-                    }
-        } else {
-            equalOrNot = false;
+        for (int i = 0; i < arrayA.length; i++) {
+            for (int j = 0; j < arrayB.length; j++) {
+                if (arrayA[i][j] == arrayB[i][j]) {
+                    equalOrNot = true;
+                } else {
+                    equalOrNot = false;
+                }
+            }
         }
-        if (equalOrNot) {
-            System.out.println("Two Arrays Are Equal");
-        } else {
-            System.out.println("Two Arrays Are Not equal");
-        }
+        return equalOrNot;
     }
 }
--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -7,14 +7,12 @@ public class ArrayTest {
         tablica1[1][0] = 1;
 
         int[][] tablica2 = new int[2][2];
-        tablica1[0][0] = 1;
-        tablica1[0][1] = 1;
-        tablica1[1][0] = 1;
-        tablica1[1][0] = 1;
+        tablica2[0][0] = 1;
+        tablica2[0][1] = 1;
+        tablica2[1][0] = 1;
+        tablica2[1][0] = 1;
 
         ArrayComparator porównanie = new ArrayComparator();
-        porównanie.compare(tablica1, tablica2);
-
+        System.out.println(porównanie.compare(tablica1, tablica2));
     }
-
 }
