f5e372a30d0199bff657d9597367a841107ac6f2
rudyknowak <rudyknowak@gmail.com>
2018-02-11 11:53:48 +0100

    poprawka 2

--- src/ArrayComparator.java
+++ src/ArrayComparator.java
@@ -1,17 +1,15 @@
 public class ArrayComparator {
 
     public boolean compare(int[][] arrayA, int[][] arrayB) {
-        boolean equalOrNot = true;
 
         for (int i = 0; i < arrayA.length; i++) {
             for (int j = 0; j < arrayB.length; j++) {
-                if (arrayA[i][j] == arrayB[i][j]) {
-                    equalOrNot = true;
-                } else {
-                    equalOrNot = false;
+
+                if (arrayA[i][j] != arrayB[i][j]) {
+                    return false;
                 }
             }
         }
-        return equalOrNot;
+        return true;
     }
 }
--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -4,15 +4,15 @@ public class ArrayTest {
         tablica1[0][0] = 1;
         tablica1[0][1] = 1;
         tablica1[1][0] = 1;
-        tablica1[1][0] = 1;
+        tablica1[1][1] = 1;
 
         int[][] tablica2 = new int[2][2];
         tablica2[0][0] = 1;
         tablica2[0][1] = 1;
         tablica2[1][0] = 1;
-        tablica2[1][0] = 1;
+        tablica2[1][1] = 1;
 
-        ArrayComparator porównanie = new ArrayComparator();
-        System.out.println(porównanie.compare(tablica1, tablica2));
+        ArrayComparator porownanie = new ArrayComparator();
+        System.out.println(porownanie.compare(tablica1, tablica2));
     }
 }
